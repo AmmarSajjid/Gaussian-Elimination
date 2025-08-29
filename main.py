50f6b86dfb6be2b6493761b08050d8a90421dcf4
Ammar Sajjid <ammarsajjidali@gmail.com>
2025-08-29 22:34:05 +0400

    refactored code and renamed the functions

--- main.py
+++ main.py
@@ -16,13 +16,10 @@ def gauss_elim(A, b):
     n = len(A[0])
     x = np.zeros((n,1))
     
-    # Forward Pass
-    for i in range(n):
-        solve_pivot(A, b, n, i)
+    # Reduced Echelon Form
+    A, b = REF(A, b, n)
 
-    # Matrix is now in REF
     # Back Substitution
-    # x[n-1] = b[n-1]
     for i in range(n-1, -1, -1):
         val_to_sub = 0
         for k in range(i+1, n):
@@ -34,7 +31,7 @@ def gauss_elim(A, b):
     return x
 
 
-def solve_pivot(A, b, n, i):
+def REF(A, b, n):
     """
     Create a pivot for i-th row and eliminate i-th column.
 
@@ -44,33 +41,29 @@ def solve_pivot(A, b, n, i):
     n (int): Size of the matrix.
     i (int): Current row index.
     """
+    for i in range(n):
+        # Creating a pivot for the i-th row
+        # Performing Ri = Ri * (1 / pivot_value)
+        pivot_value = A[i][i]
+        A[i] = A[i] / pivot_value
+        b[i] = b[i] / pivot_value
 
-    # Creating a pivot for the i-th row
-    # Performing Ri = Ri * (1 / pivot_value)
-    pivot_value = A[i][i]
-    A[i] = A[i] / pivot_value
-    b[i] = b[i] / pivot_value
+        # Eliminating the i-th column
+        for j in range(i+1, n):
+            val = A[j][i]
+            # perform Rj = Rj - val * Ri
+            A[j] = A[j] - (val * A[i])
+            b[j] = b[j] - (val * b[i])    
 
-    # Eliminating the i-th column
-    for j in range(i+1, n):
-        val = A[j][i]
-        # perform Rj = Rj - val * Ri
-        A[j] = A[j] - (val * A[i])
-        b[j] = b[j] - (val * b[i])    
-
+    
     return A, b
-
-    
-    
-
-
 
 
 if __name__ == "__main__":
     # Example usage
-    n = 2
-    A = np.array([[1, 1], [1, -1]], dtype=float)
-    b = np.array([5, 1], dtype=float).reshape(n,1)
+    n = 3
+    A = np.array([[2, -1, 1], [1, 3, 2], [1, -1, 2]], dtype=float)
+    b = np.array([3, 13, 5], dtype=float).reshape(n,1)
 
     x = gauss_elim(A,b)
     print(x)
